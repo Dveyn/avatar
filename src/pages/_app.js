0802af818cfe04b22d0c19e4b0afe4ba79c81fdb
Дмитрий Ананьев <adm.saitov1@yandex.ru>
2025-02-16 20:04:56 +0300

    Добвил метрику

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,11 @@
 import { Footer, Header } from "@@/components/layout";
+import YandexMetrika from "@@/components/layout/YandexMetrika/YandexMetrika";
 import "@@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <YandexMetrika />
       <Header />
       <Component { ...pageProps } />
       <Footer />
